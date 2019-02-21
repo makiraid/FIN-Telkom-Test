11396b91ef591b960f8a25787ff6d669c6334701
Risky Makira <kirahaxor20@gmail.com>
2019-02-21 13:13:38 +0700

    FIN Telkom Test

--- 2.php
+++ 2.php
@@ -3,7 +3,8 @@ $number = '01448920394027742900';
 if(ctype_digit($number) && strlen($number) == 20) {
   $number = substr($number, 0, 4) .'-'.
             substr($number, 4, 4) .'-'.
-            substr($number, 4, 4) .'-'.
-            substr($number, 4, 4);
+            substr($number, 8, 4) .'-'.
+            substr($number, 12, 4) .'-'.
+            substr($number, 16, 4);
 echo $number;
 }
